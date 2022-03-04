21e00237efc86c2d4a82f48c813b0622d290d5e3
immadisairaj <immadirajendra.sai@gmail.com>
2022-03-04 18:03:24 +0530

    precache image in about

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "980547175e325fe622a3362b84d55b6a",
-"index.html": "ff40638df12153412225fc1589a07568",
-"/": "ff40638df12153412225fc1589a07568",
-"main.dart.js": "0d9296256a87d62ceb34cad789400286",
+"index.html": "b759393472d50d47f3824e576c520481",
+"/": "b759393472d50d47f3824e576c520481",
+"main.dart.js": "91a877e7671ca961cb34c9d6ceb0a427",
 "logo.png": "639b56a22e988dce81d486a3ad21ff71",
 "manifest.json": "90c454c29b48f08bc06627d125f70466",
 "assets/AssetManifest.json": "63e497b5096ded4838fff49fe9012fe5",
